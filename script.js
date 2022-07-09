c5bcb0430896e1ea3e4a6406bbf4feb50141035c
Felipe <felipe__andrew@hotmail.com>
2022-07-09 20:43:37 -0300

    Requisitos 10, 11 e 12

--- script.js
+++ script.js
@@ -1,61 +1,94 @@
-const black = document.querySelector('.color-1')
-black.classList.add('color-1')
+// cor 1
+const black = document.querySelector('.color-1');
+black.classList.add('color-1');
 black.classList.add('selected');
 
-const purple = document.querySelector('.color-2')
-purple.classList.add('color-2')
+// cores aleatórias
+const random2 = document.querySelector('.color-2');
+random2.style.backgroundColor = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')'
 
-const blueViolet = document.querySelector('.color-3')
-blueViolet.classList.add('color-3')
+const random3 = document.querySelector('.color-3');
+random3.style.backgroundColor = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')' 
 
-const brown = document.querySelector('.color-4')
-brown.classList.add('color-4')
+const random4 = document.querySelector('.color-4');
+random4.style.backgroundColor = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')' 
 
-const board = document.querySelector('#pixel-board')
 
-function pickColor (event) {
-    const selecionada = document.querySelector('.selected')
-    selecionada.classList.remove('selected')
-    event.target.classList.add('selected')
+// criar quadro dinamicamente
+const board = document.querySelector('#pixel-board');
+
+function createSquare(sizeInput){
+    const squareDefault = sizeInput *  50;
+    const widtHeight = sizeInput * sizeInput;
+    board.style.width = squareDefault + 'px';
+    board.style.height = squareDefault + 'px';
+
+    for(let i = 0; i < widtHeight; i += 1) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('pixel');
+        board.appendChild(newDiv);
+    }
 }
 
-function changeColor(cor) {
-    const teste = document.querySelector('.selected')
-    const colorChanged = window.getComputedStyle(teste).getPropertyValue('background-Color');
+createSquare(5);
+
+// pinta com cor padrão
+function paint(cor) {
+    const change = document.querySelector('.selected');
+    const colorChanged = window.getComputedStyle(change).getPropertyValue('background-Color');
     
     if ( cor.target.classList.contains('pixel')) {
-        cor.target.style.backgroundColor = colorChanged
+        cor.target.style.backgroundColor = colorChanged;
     }
 }
 
-board.addEventListener ('click', changeColor);
+// escolhe a cor
+function pickColor (event) {
+    const selecionada = document.querySelector('.selected');
+    selecionada.classList.remove('selected');
+    event.target.classList.add('selected');
+}
 
-black.addEventListener('click', pickColor)
-purple.addEventListener('click', pickColor)
-blueViolet.addEventListener('click', pickColor)
-brown.addEventListener('click', pickColor)
+// escutadores de evento
+board.addEventListener ('click', paint);
+black.addEventListener('click', pickColor);
+random2.addEventListener('click', pickColor);
+random3.addEventListener('click', pickColor);
+random4.addEventListener('click', pickColor);
 
+// limpa quadrados pintados
 function clearBoard() {
-    const limpar = document.querySelectorAll('.pixel')
+    const limpar = document.querySelectorAll('.pixel');
     for (let i = 0; i < limpar.length; i += 1) {
-        limpar[i].style.backgroundColor = 'white'
+        limpar[i].style.backgroundColor = 'white';
     }
 }
 
-function changeSize() {
-    const colorSquare = document.querySelectorAll('.pixel');
-    
-    let input = document.querySelector('#board-size')
-    let numero = input.value
-    numero *= numero
+const input = document.querySelector('#board-size')
 
-    if(numero === '' || numero === 0) {
-        window.alert('Board inválido!')
-    } else {
-        for (let i = 0; i < colorSquare.length; i += 1) {
-            board.style.width = numero+'px'
-            board.style.height = numero+'px'
-        }
+// remove quadrados anteriores
+function removeSquare() {
+    const removeAll = document.querySelectorAll('.pixel');
+    for (let i = 0; i < removeAll.length; i += 1) {
+        removeAll[i].remove();
     }
-   clearBoard()
+}
+
+// altera tamanho do quadro
+function changeSize() {
+    let number = input.value;
+    if (number === '' || number === 0) {
+        window.alert('Board inválido!')
+    }
+
+    if (number < 5) {
+        number = 5;
+    }
+
+    if (number > 50) {
+        number = 50;
+    }
+    removeSquare();
+    createSquare(number);
+    clearBoard()
 }
